2b7b68b4f3be483fc41e324131f807e96e771b0e
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 16:33:13 -0700

    added high score functionality

--- game.py
+++ game.py
@@ -5,25 +5,46 @@ import random
 print "Hi player!"
 name = raw_input("What is your name? ")
 game_try = raw_input("Do you want to play a game? Enter Y for Yes or hit any other key for No. ")
+high_score = None
+
+#this while loop controls if someone wants to play the game.
 while game_try == "Y":
     number = random.randint(1, 100)
 # print number
     guess = 0
 # print guess
+    current_score = []
+    
+    # this while loop is the main game
     while guess != number:
         guess = raw_input("%s please choose a number between 1 and 100. " % (name))
+        current_score.append(guess) # append guesses to an empty list to count later
+        # print high_score
+
+
+        #validates if the input is a digit
         if guess.isdigit():
             guess = int(guess)
             if guess >= 1 and guess <= 100:
                 if guess < number:
-                    print "That was too low."
+                    print "%i was too low." % (guess)
                 elif guess > number:
-                    print "That was too high."
+                    print "%i was too high." % (guess)
                 else: 
                     print "Good job, %s! %i was the number." % (name, guess)
+                    print "It took you %i guesses." % (len(current_score))
+                    if high_score == None:
+                        high_score = len(current_score)
+                    else:
+                        if high_score > len(current_score):
+                            high_score = len(current_score)
+                    print "The current high score is %i." % (high_score)
+                    # asks player whether they want to play again and go through the main loop
+                    
                     game_try = raw_input("Would you like to play again? Enter Y for Yes or any other key to exit. ")
             else:
                 print "Please choose a number between 1 and 100."
+        # if it's not a number, tells them 
         else:
-            print "Hey %s, %s is not a number." % (name, guess)
+            print "Hey %s, %s is not a number." % (name, guess) 
 print "Okay maybe another time."
