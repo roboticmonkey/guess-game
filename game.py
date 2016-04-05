811c8ece79f7bdeb7b8e095c4fa8921e6a833349
Hackbright Student <info@hackbrightacademy.com>
2016-04-05 15:22:02 -0700

    finished game need +100

---
+++ game.py
@@ -0,0 +1,24 @@
+# Put your code here
+import random
+
+
+print "Hi player!"
+name = raw_input("What is your name? ")
+
+number = random.randint(1, 100)
+# print number
+guess = 0
+# print guess
+while guess != number:
+    guess = raw_input("%s please choose a number between 1 and 100. " % (name))
+    if guess.isdigit():
+        guess = int(guess)
+        if guess < number:
+            print "That was too low."
+        elif guess > number:
+            print "That was too high."
+        else: 
+            print "Good job, %s! %i was the number." % (name, guess)
+    else:
+        print "Hey %s, %s is not a number." % (name, guess)
+       
